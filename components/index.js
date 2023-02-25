dc6bacb5e24e48b1046b9610dec3d36b5d278b05
Jameson Chen <[jamesonychan@gmail.com]>
2023-02-25 15:21:18 -0500

    * app steup * Added Home and Details screens

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import HomeHeader from "./homeHeader";
+import FocusedStatusBar from "./focusedStatusBar";
+import NftCard from "./nftCard";
+import { CircleButton, RectButton } from "./button";
+
+export { HomeHeader, NftCard, FocusedStatusBar, CircleButton, RectButton };
---
+++ screens/home.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Text, View, SafeAreaView, FlatList } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+import { NftCard, HomeHeader, FocusedStatusBar } from "../components";
+
+function Home() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar backgroud={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            style={{ zIndex: 0 }}
+            data={NFTData}
+            renderItem={({ item }) => <NftCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <View style={{ height: 300, backgroundColor: COLORS.primary }}></View>
+        <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Home;
