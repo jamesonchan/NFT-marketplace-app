a5f019d043f9da3778b16388939b887d2e7a0a1a
Jameson Chen <[jamesonychan@gmail.com]>
2023-02-27 11:53:35 -0500

    * Complete Details screen and its components

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import HomeHeader from "./homeHeader";
 import FocusedStatusBar from "./focusedStatusBar";
 import NftCard from "./nftCard";
 import { CircleButton, RectButton } from "./button";
+import DetailsDesc from "./detailsDesc";
+import DetailsBid from "./detailsBid";
+import { SubInfo } from "./subInfo";
 
-export { HomeHeader, NftCard, FocusedStatusBar, CircleButton, RectButton };
+export {
+  HomeHeader,
+  NftCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
